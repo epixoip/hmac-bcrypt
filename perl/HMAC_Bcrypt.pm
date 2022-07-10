9e92da8ddc32419a27673eb5569a3a15edcc5ffb
Jeremi M Gosney <deadbird@gmail.com>
2022-07-10 04:40:04 -0500

    add perl example

---
+++ perl/HMAC_Bcrypt.pm
@@ -0,0 +1,79 @@
+package HMAC_Bcrypt;
+
+use strict;
+use warnings;
+
+use Exporter::Auto;
+
+use Crypt::Eksblowfish::Bcrypt qw(bcrypt en_base64);
+use String::Compare::ConstantTime qw(equals);
+use Digest::SHA qw(hmac_sha512_base64);
+use Crypt::URandom qw(urandom);
+
+use constant BCRYPT_ID         => '2a';
+use constant BCRYPT_COST       => 13;
+use constant BCRYPT_SALT_BYTES => 16;
+use constant BCRYPT_PEPPER     => 'hmac_bcrypt';
+
+sub hmac_bcrypt_new {
+    # generates a new hash from a plaintext password
+    #
+    # @param password: scalar containing plaintext password
+    # @param settings: scalar containing settings string or undef
+    # @param pepper: scalar containing pepper string or undef
+    # @returns hashed password or croak
+
+    my ($password, $settings, $pepper) = @_;
+    my ($cost, $salt);
+
+    if ($settings) {
+        (undef, undef, $cost, $salt) = split /\$/, $settings;
+    }
+
+    unless (length $cost) {
+        $cost = BCRYPT_COST;
+    }
+
+    unless (length $salt && length $salt >= 22) {
+        $salt = en_base64(
+            urandom(BCRYPT_SALT_BYTES)
+        );
+    } else {
+        $salt = substr($salt, 0, 22);
+    }
+
+    $settings = sprintf('$%2s$%02d$%s', BCRYPT_ID, $cost, $salt);
+
+    my $pre_hash  = hmac_sha512_base64($password, $pepper);
+    my $mid_hash  = bcrypt($pre_hash, $settings);
+    my $post_hash = hmac_sha512_base64($mid_hash, $pepper);
+
+    return $settings . $post_hash;
+}
+
+sub hmac_bcrypt_compare {
+    # compares password to stored hash value
+    #
+    # @param password: scalar containing plaintext password
+    # @param valid: scalar containing stored hash value
+    # @param pepper: scalar containing pepper string or undef
+    # @returns boolean
+
+    my ($password, $valid, $pepper) = @_;
+
+    my $hash = hmac_bcrypt_new($password, $valid, $pepper);
+    return equals($hash, $valid);
+}
+
+INIT {
+    my $pass   = 'test-password';
+    my $pepper = 'test-pepper';
+    
+    my $hash = hmac_bcrypt_new($pass, undef, $pepper);
+    
+    unless (hmac_bcrypt_compare($pass, $hash, $pepper)) {
+        die "HMAC_Bcrypt module failed sanity check!";
+    }
+}
+
+1;
