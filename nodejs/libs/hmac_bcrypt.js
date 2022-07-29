ec8af7887c7d2ed7dfa3a71a38a8df5f3d668c90
Jeremi M Gosney <deadbird@gmail.com>
2022-07-29 03:26:02 -0500

    simplify js settings

--- nodejs/libs/hmac_bcrypt.js
+++ nodejs/libs/hmac_bcrypt.js
@@ -28,8 +28,7 @@ export function hmac_bcrypt_hash(password, settings, pepper = BCRYPT_PEPPER) {
     if (!salt) {
         settings = bcrypt.genSaltSync(parseInt(cost), BCRYPT_ID)
     } else {
-        salt = salt.substring(0, 22)
-        settings = `\$2${BCRYPT_ID}\$${cost}\$${salt}`
+        settings = settings.substring(0, 29)
     }
 
     let pre_hash = crypto.createHmac("sha512", pepper)
