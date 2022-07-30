3ab7894527e9d8f2eb786ec92efcc1136ae0cd69
Brandon <bperry.volatile@gmail.com>
2022-07-30 08:17:11 -0500

    refactored to better match other implementations

---
+++ csharp/bcrypt-hmac/HMAC.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace bcrypt_hmac
+{
+    static class HMAC
+    {
+
+        public static string BCRYPT_ID  = "2a";
+        public static int BCRYPT_COST = 13;
+        public static string BCRYPT_PEPPER = "hmac_bcrypt";
+
+        public static string hmac_bcrypt_hash(string password, string? settings = null, string? pepper = null)
+        {
+            if (pepper == null)
+                pepper = BCRYPT_PEPPER;
+
+            string[]? sets = null;
+
+            if (!string.IsNullOrEmpty(settings))
+                sets = settings.Split('$');
+            
+            string id;
+            string cost;
+            string salt;
+
+            if (sets != null && sets.Length > 0)
+            {
+                id = sets[1];
+                cost = sets[2];
+
+                if (sets.Length > 3)
+                    salt = sets[3][..22];
+                else
+                    salt = BCrypt.Net.BCrypt.GenerateSalt();
+            }
+            else
+            {
+                id = BCRYPT_ID;
+                cost = BCRYPT_COST.ToString();
+                salt = BCrypt.Net.BCrypt.GenerateSalt();
+            }
+
+            string cryptAmble = "$" + id + "$" + cost + "$" + salt;
+            HMACSHA512 hmac = new HMACSHA512(Encoding.UTF8.GetBytes(pepper));
+            string prehash = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password)));
+            string midhash = BCrypt.Net.BCrypt.HashPassword(prehash, cryptAmble);
+
+            hmac = new HMACSHA512(Encoding.UTF8.GetBytes(pepper));
+            string final = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(midhash))).Replace("=", string.Empty);
+
+            return cryptAmble + final;
+        }
+
+        public static bool hmac_bcrypt_verify(string password, string expectedHash, string pepper)
+        {
+            string tmp = hmac_bcrypt_hash(password, expectedHash, pepper);
+
+            byte[] newHash = Encoding.UTF8.GetBytes(tmp);
+            byte[] hash = Encoding.UTF8.GetBytes(expectedHash);
+
+            bool eq = false;
+            if (newHash.Length == hash.Length)
+            {
+                int i = 0;
+                while ((i < newHash.Length) && (newHash[i] == hash[i]))
+                    i += 1;
+                
+                if (i == newHash.Length)
+                    eq = true;
+            }
+
+            return eq;
+        }
+    }
+}
---
+++ csharp/bcrypt-hmac/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using bcrypt_hmac;
+using System.Text;
+
+string password = "test-pass";
+string hmacPepper = "test-pepper";
+string final = "$2a$13$v.vnO5oVlX/5zJM9TTXSz.JMdh9WwErhl6x9XMOEBs5x1R1FxuPC29TMJSMeAEnUlkEgbZw6r0FFZ9jFN07eykXAMgNZH3WrZSqxQkj4qKEQ";
+
+
+if (HMAC.hmac_bcrypt_verify(password, final, hmacPepper))
+    Console.WriteLine("Verified with Time Safety");
+else
+    Console.WriteLine("Not verified");
