36eb3e1a2b27e8849e393f6fe09c4c833fc70667
Jeremi M Gosney <deadbird@gmail.com>
2022-07-30 17:37:22 -0500

    clean up csharp

---
+++ csharp/hmac-bcrypt/HMAC_Bcrypt.cs
@@ -0,0 +1,78 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace hmac_bcrypt
+{
+    static class HMAC_Bcrypt
+    {
+        private const int    BCRYPT_COST   = 13;
+        private const string BCRYPT_PEPPER = "hmac_bcrypt";
+
+        public static string hmac_bcrypt_hash(string password, string? settings = null, string? pepper = null)
+        {
+            int cost = BCRYPT_COST;
+            string? salt = null;
+
+            if (!string.IsNullOrEmpty(settings)) {
+                string[] sets = settings.Split('$');
+
+                cost = Int16.Parse(sets[2]);
+
+                if (sets.Length > 3 && !string.IsNullOrEmpty(sets[3])) {
+                    salt = sets[3];
+                }
+            }
+
+            if (string.IsNullOrEmpty(salt)) {
+                settings = BCrypt.Net.BCrypt.GenerateSalt(cost);
+            } else if (settings != null) {
+                settings = settings[..29];
+            }
+
+            if (string.IsNullOrEmpty(pepper)) {
+                pepper = BCRYPT_PEPPER;
+            }
+
+            HMACSHA512 hmac = new HMACSHA512(
+                Encoding.UTF8.GetBytes(pepper)
+            );
+
+            string pre_hash = Convert.ToBase64String(
+                hmac.ComputeHash(
+                    Encoding.UTF8.GetBytes(password)
+                )
+            );
+
+            string mid_hash = BCrypt.Net.BCrypt.HashPassword(pre_hash, settings);
+
+            string final = Convert.ToBase64String(
+                hmac.ComputeHash(
+                    Encoding.UTF8.GetBytes(mid_hash)
+                )
+            ).Replace("=", string.Empty);
+
+            return settings + final;
+        }
+
+        public static bool hmac_bcrypt_verify(string password, string valid, string? pepper = null)
+        {
+            if (string.IsNullOrEmpty(pepper)) {
+                pepper = BCRYPT_PEPPER;
+            }
+
+            byte[] a = Encoding.UTF8.GetBytes(
+                hmac_bcrypt_hash(password, valid, pepper)
+            );
+
+            byte[] b = Encoding.UTF8.GetBytes(valid);
+
+            uint diff = (uint) a.Length ^ (uint) b.Length;
+
+            for (int i = 0; i < a.Length && i < b.Length; i++) {
+                diff |= (uint)(a[i] ^ b[i]);
+            }
+
+            return diff == 0;
+        }
+    }
+}
---
+++ csharp/hmac-bcrypt/Program.cs
@@ -0,0 +1,36 @@
+﻿using hmac_bcrypt;
+using System.Text;
+
+const string password = "test-pass";
+const string pepper   = "test-pepper";
+const string expected = "$2a$13$v.vnO5oVlX/5zJM9TTXSz.JMdh9WwErhl6x9XMOEBs5x1R1FxuPC29TMJSMeAEnUlkEgbZw6r0FFZ9jFN07eykXAMgNZH3WrZSqxQkj4qKEQ";
+
+string test = string.Empty;
+
+test = HMAC_Bcrypt.hmac_bcrypt_hash(password);
+
+if (!System.Text.RegularExpressions.Regex.IsMatch(test, @"^\$2a\$[0-9]{2}\$[.\\/+A-Za-z0-9]{108}$"))
+    throw new Exception("Only password test failed");
+
+Console.WriteLine("Verified password-only hashing");
+
+
+test = HMAC_Bcrypt.hmac_bcrypt_hash(password, "$2a$10$");
+
+if (!System.Text.RegularExpressions.Regex.IsMatch(test, @"^\$2a\$10\$[.\\/+A-Za-z0-9]{108}$"))
+    throw new Exception("Password + cost test failed");
+
+Console.WriteLine("Verified password + cost hashing");
+
+
+test = HMAC_Bcrypt.hmac_bcrypt_hash(password, "$2a$10$v.vnO5oVlX/5zJM9TTXSz.");
+
+if (!System.Text.RegularExpressions.Regex.IsMatch(test, @"^\$2a\$10\$v\.vnO5oVlX/5zJM9TTXSz\.[.\\/+A-Za-z0-9]{86}$"))
+    throw new Exception("Password + cost + salt test failed");
+
+Console.WriteLine("Verified password + cost + salt hashing");
+
+if (HMAC_Bcrypt.hmac_bcrypt_verify(password, expected, pepper))
+    Console.WriteLine("Verified with Time Safety");
+else
+    Console.WriteLine("Not verified");
